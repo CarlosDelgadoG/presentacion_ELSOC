0441f63f4bfd4cf6a5f840469e59b9493a3d2850
CarlosDelgadoG <carlos.delgado@ug.uchile.cl>
2022-06-23 19:05:56 -0400

    cambio portada y orden graficos

--- MODELOS/pruebas con lags gee.R
+++ MODELOS/pruebas con lags gee.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-library(gee)
+library(geepack)
 
 gee_LAG_m0_sexo_fac <- readRDS("MODELOS/gee_LAG_m0_sexo_fac.RDS")
 gee_LAG_s12_l <- readRDS("MODELOS/gee_LAG_s12_l.RDS")
@@ -21,3 +21,6 @@ gee_LAG_s_imc_lc%>%
 
 gee_LAG_s04_l%>%
   summary()
+
+list(gee_LAG_m0_sexo_fac,gee_LAG_s12_l,gee_LAG_m01_fac,gee_LAG_s_imc_lc,gee_LAG_s04_l)%>%
+  texreg::knitreg(single.row=TRUE)
